8a5ef93522a4bd8c4c05118767c8e226b5a1e51e
Roberto Luiz <luizroberto1957@hotmail.com>
2021-01-11 16:10:02 -0300

    chore: First Commit

---
+++ CEP_DLL/CEP_DLL/Consulta_CEP.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+namespace CEP_DLL
+{
+    public class Consulta_CEP
+    {
+        public Consulta_CEP() { }
+        public string Consulta_api(string cep)
+        {
+            
+            if (cep.Length == 8)
+            {
+                try
+                {
+                    RestClient restClient = new RestClient(string.Format("https://viacep.com.br/ws/{0}/json/", cep));
+                    RestRequest restRequest = new RestRequest(Method.GET);
+
+                    IRestResponse restResponse = restClient.Execute(restRequest);
+
+                    if (restResponse.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                        //MessageBox.Show("Houve um problema com sua requisição: " + restResponse.Content, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return "MsgBox:Houve um problema com sua requisição: " + restResponse.Content;
+                    else
+                    {
+                        DadosRetorno dadosRetorno = new JsonSerializer().Deserialize<DadosRetorno>(restResponse);
+
+                        if (dadosRetorno.cep is null)
+                        {
+                            //MessageBox.Show("CEP não encontrado!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            return "MsgBox:CEP não encontrado!";
+                        }
+
+                        //string txtCep = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.cep);
+                        //string txtEndereco = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.logradouro);
+                        //string txtComp = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.complemento);
+                        //string txtBairro = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.bairro);
+                        //string txtLocalidade = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.localidade);
+                        //string txtUF = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.uf);
+                        //string txtUnid = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.unidade);
+                        //string txtIBGE = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.ibge);
+                        //string txtGIA = ObterStringSemAcentosECaracteresEspeciais(dadosRetorno.gia);
+
+                        string txtCep = dadosRetorno.cep;
+                        string txtEndereco = dadosRetorno.logradouro;
+                        string txtComp = dadosRetorno.complemento;
+                        string txtBairro = dadosRetorno.bairro;
+                        string txtLocalidade = dadosRetorno.localidade;
+                        string txtUF = dadosRetorno.uf;
+                        string txtUnid = dadosRetorno.unidade;
+                        string txtIBGE = dadosRetorno.ibge;
+                        string txtGIA = dadosRetorno.gia;
+
+
+                        // Devolução da Consulta de CEP
+                        //Create_txt(txtcep, txtEndereco, txtComp, txtBairro, txtLocalidade, txtUF, txtUnid, txtIBGE, txtGIA, arquivotxt);
+                        return txtCep + ";" + txtEndereco + ";" + txtComp + ";" + txtBairro + ";" + txtLocalidade + ";" + txtUF + ";" + txtUnid + ";" + txtIBGE + ";" + txtGIA;
+                    }
+                }
+                catch (Exception erro)
+                {
+                    //MessageBox.Show("Erro geral ao consulta a API: " + erro.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return "MsgBox:Erro geral ao consulta a API: " + erro.Message;
+                }
+            }
+            else
+            {
+                //MessageBox.Show("Formato do CEP invalido, o CEP deve possuir 8 caracteres!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return "MsgBox:Formato do CEP invalido, o CEP deve possuir 8 caracteres!";
+            }
+
+        }
+
+
+        class DadosRetorno
+        {
+            public string cep { get; set; }
+            public string logradouro { get; set; }
+            public string complemento { get; set; }
+            public string bairro { get; set; }
+            public string localidade { get; set; }
+            public string uf { get; set; }
+            public string unidade { get; set; }
+            public string ibge { get; set; }
+            public string gia { get; set; }
+
+        }
+        //Metodo utilizado na versão exe 
+        public void Create_txt(string cep, string logradouro, string complemento, string bairro, string localidade, string uf, string unidade, string ibge, string gia, string arquivotxt)
+        {
+            string destino = GetIni.GetIniValue("BANCO DE DADOS", "PATH", "QIClass.ini");
+
+            //StreamWriter escritor = new StreamWriter(destino + "Textos\\ConsultaCEP_ViaCEP.txt");
+            StreamWriter escritor = new StreamWriter(destino + "Textos\\" + arquivotxt);
+
+
+            escritor.WriteLine("CEP;ENDERECO;COMPLEMENTO;BAIRRO;LOCALIDADE;UF;UNIDADE;IBGE;GIA");
+            escritor.WriteLine(cep + ";" + logradouro + ";" + complemento + ";" + bairro + ";" + localidade + ";" + uf + ";" + unidade + ";" + ibge + ";" + gia);
+
+            escritor.Close();
+
+        }
+        public static string ObterStringSemAcentosECaracteresEspeciais(string str)
+        {
+            if (str is null)
+            {
+                return str;
+            }
+            else
+            {
+                /** Troca os caracteres acentuados por não acentuados **/
+                string[] acentos = new string[] { "ç", "Ç", "á", "é", "í", "ó", "ú", "ý", "Á", "É", "Í", "Ó", "Ú", "Ý", "à", "è", "ì", "ò", "ù", "À", "È", "Ì", "Ò", "Ù", "ã", "õ", "ñ", "ä", "ë", "ï", "ö", "ü", "ÿ", "Ä", "Ë", "Ï", "Ö", "Ü", "Ã", "Õ", "Ñ", "â", "ê", "î", "ô", "û", "Â", "Ê", "Î", "Ô", "Û" };
+                string[] semAcento = new string[] { "c", "C", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "a", "o", "n", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "A", "O", "N", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+
+                for (int i = 0; i < acentos.Length; i++)
+                {
+                    str = str.Replace(acentos[i], semAcento[i]);
+                }
+                /** Troca os caracteres especiais da string por "" **/
+                string[] caracteresEspeciais = { "¹", "²", "³", "£", "¢", "¬", "º", "¨", "\"", "'", ".", ",", "-", ":", "(", ")", "ª", "|", "\\\\", "°", "_", "@", "#", "!", "$", "%", "&", "*", ";", "/", "<", ">", "?", "[", "]", "{", "}", "=", "+", "§", "´", "`", "^", "~" };
+
+                for (int i = 0; i < caracteresEspeciais.Length; i++)
+                {
+                    str = str.Replace(caracteresEspeciais[i], "");
+                }
+
+                /** Troca os caracteres especiais da string por " " **/
+                str = Regex.Replace(str, @"[^\w\.@-]", " ",
+                                    RegexOptions.None, TimeSpan.FromSeconds(1.5));
+
+                return str.Trim();
+            }
+        }
+
+    }
+}
---
+++ CEP_DLL/CEP_DLL/GetIni.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace CEP_DLL
+{
+    public class GetIni
+    {
+        // Declaração das funções não gerenciadas: GetPrivateProfileString e 
+        // WritePrivateProfileString
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, int nSize, string lpFileName);
+
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern bool WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+        public static string PathIni(string nomeArquivo)
+        {
+            string SystemRoot, UserProfile;
+            bool WinIni, UserIni;
+            //seta variáveis da function com as variáveis do ssitema operacional
+            SystemRoot = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+            UserProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            //verifica se o ini está no c:\windows
+            WinIni = File.Exists(SystemRoot + "\\" + nomeArquivo);
+            if (WinIni == true)
+            {
+                nomeArquivo = SystemRoot + "\\";
+            }
+            //verifica se o ini está no diretório de perfil do usuário
+            else
+            {
+                UserIni = File.Exists(UserProfile + "\\" + nomeArquivo);
+                if (UserIni == true)
+                {
+                    nomeArquivo = UserProfile + "\\";
+                }
+                else
+                {
+
+                }
+            }
+            return nomeArquivo;
+        }
+
+        public static string GetIniValue(string section, string key, string nomeArquivo)
+        {
+            int chars = 256;
+            StringBuilder buffer = new StringBuilder(chars);
+            string sDefault = "";
+            if (nomeArquivo == "QIClass.ini")
+            {
+                nomeArquivo = PathIni(nomeArquivo) + nomeArquivo;
+            }
+
+            if (GetPrivateProfileString(section, key, sDefault, buffer, chars, nomeArquivo) != 0)
+            {
+                return buffer.ToString();
+            }
+            else
+            {
+                // Verifica o último erro Win32.
+                int err = Marshal.GetLastWin32Error();
+                return null;
+            }
+        }
+
+        public static bool WriteIniValue(string section, string key, string value, string nomeArquivo)
+        {
+            return WritePrivateProfileString(section, key, value, nomeArquivo);
+        }
+
+        public static string getCaminhoArquivoINI(string caminhoArquivo)
+        {
+            if (caminhoArquivo.IndexOf("\\bin\\Debug") != -1)
+            {
+                caminhoArquivo = caminhoArquivo.Replace("\\bin\\Debug", "");
+            }
+            else if (caminhoArquivo.IndexOf("\\bin\\Release") != -1)
+            {
+                caminhoArquivo = caminhoArquivo.Replace("\\bin\\Release", "");
+            }
+            return caminhoArquivo;
+        }
+    }
+}
